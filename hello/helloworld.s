4db2154568a3a0e6b6240868407ab3b5398edb70
junsu yun <yunjs24@gmail.com>
2022-02-03 01:04:33 +0900

    hello world!

---
+++ hello/helloworld.s
@@ -0,0 +1,17 @@
+
+
+section .data
+		msg db "Hellow World"
+
+section .text
+		global_start
+
+_start:
+		mov	rax, 1
+		mov rdi, 1
+		mov rsi, msg
+		mov rdx, 12
+		syscall
+		mov rax, 60
+		mov rdi, 0
+		syscall
